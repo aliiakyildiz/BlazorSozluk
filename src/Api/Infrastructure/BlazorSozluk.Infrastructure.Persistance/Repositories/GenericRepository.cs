ffb18e774189e318561ef79069b6e927bcce9957
Your NameAliAkyildiz <ali.akyildiz.94@gmail.com>
2022-07-16 16:43:09 +0300

    Added generic repository.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistance/Repositories/GenericRepository.cs
@@ -0,0 +1,150 @@
+﻿using BlazorSozluk.Api.Application.Interfaces.Repositories;
+using BlazorSozluk.Api.Domain.Models;
+using BlazorSozluk.Infrastructure.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistance.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly BlazorSozlukContext dbContext;
+
+        protected DbSet<TEntity> entity => dbContext.Set<TEntity>();
+        public GenericRepository(BlazorSozlukContext dbContext)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentException(nameof(dbContext));
+        }
+
+        #region Insert Methods
+        public virtual async Task<int> AddAsync(TEntity entity)
+        {
+            await this.entity.AddAsync(entity);
+            return await dbContext.SaveChangesAsync();
+        }
+        public virtual int Add(TEntity entity)
+        {
+            this.entity.Add(entity);
+            return dbContext.SaveChanges();
+        }
+
+        public int Add(IEnumerable<TEntity> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        #endregion
+
+
+        public int AddOrUpdate(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> AddOrUpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> AsQueryable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BulkAdd(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BulkDelete(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BulkDelete(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BulkDeleteById(IEnumerable<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task BulkUpdate(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRange(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteRangeAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetAll(bool noTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TEntity>> GetList(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> predicate, bool noTracking = true, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
